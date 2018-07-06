ae252b1170298f65b8b0db755dc5f97b4959351e
Jean Carlos Ospino Gonzalez <devjc23@gmail.com>
2018-07-05 20:41:44 -0500

    Configurado body-parser para express

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import app from "./app"
+import bodyParser from "body-parser"
+import usersRoutes from "./routes/users"
+
 const PORT = 3000
 
-app.listen(3000, () => "Server running at " + PORT)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use("/api", usersRoutes)
+
+app.listen(3000, () => console.log("Server running at " + PORT))
